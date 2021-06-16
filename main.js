c1f9e917ab5058892495e168ab0e81b286fc4e1f
Anshika2412oss <76035744+Anshika2412oss@users.noreply.github.com>
2021-06-16 15:34:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,29 @@ Webcam.set({
 camera = document.getElementById("camera");
 
 Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+    });
+}
+console.log("ml5 version" , ml5.version);
+classifier= ml5.imageClassifier('MobileNet', modelLoaded);
+function modelLoaded(){
+    console.log("Model Loaded!");
+
+}
+function verify(){
+    img= document.getElementById('capture_image');
+    classifier.classify(img, getResult);
+
+}
+function getResult(error,results){
+    if (error) {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML= results[0].label;
+    }
+}
